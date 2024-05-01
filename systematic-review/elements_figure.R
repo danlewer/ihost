60f10235687a4aa84c34729fb28ec06f2407965a
Dan Lewer <danlewer@users.noreply.github.com>
2024-05-01 16:22:38 +0100

    Update elements_figure.R

--- systematic-review/elements_figure.R
+++ systematic-review/elements_figure.R
@@ -3,6 +3,15 @@ library(RColorBrewer)
 # read data
 
 d <- read.csv("https://raw.githubusercontent.com/danlewer/ihost/main/systematic-review/elements.csv")
+# d$country <- factor(d$country, c('United States', 'Canada', 'United Kingdom'))
+# d <- d[order(d$country, -d$study_year),]
+# d$country <- as.character(d$country)
+
+# y positions
+
+d <- d[nrow(d):1,]
+d$y <- 1:nrow(d)
+d$y <- d$y + cumsum(as.integer(d$country != c('United Kingdom', d$country[-length(d$country)])) * 2)
 
 # matrix of studies vs elements
 
@@ -20,7 +29,7 @@ mat <- sapply(elements, function (x) grepl(x, d$elements))
 spl <- lapply(d$elements, strsplit, split = ';', fixed = T)
 spl <- lapply(spl, unlist)
 spl <- lapply(spl, function (x) x %in% elements)
-sapply(spl, all) # should all be true
+table(sapply(spl, all)) # should all be true
 
 # analysis of elements
 
@@ -37,24 +46,24 @@ type <- cbind(type, total = colSums(mat))
 type[type == 0] <- NA_integer_
 
 element_labels <- c("Guidance or\nprotocol",
-                    "Continuity of OAT\nfrom community to\nhospital",
-                    "Continuity of OAT\nfrom hospital to\ncommunity",
-                    "Training and\neducation in\nrelation to OST",
+                    "Measures to improve\ncontinuity of\npre-existing OAT",
+                    "Measures to improve\ncontinuity after\ninitiation in hospital",
+                    "Training and\neducation in\nrelation to OAT",
                     "Peer support\nfor patients",
-                    "Multidisciplinary\nworking to\nimprove OST",
+                    "Multidisciplinary\npatient\nreview",
                     "Electronic\nworkflow")
 
 cols <- brewer.pal(5, 'Pastel1')
 
-png('element_type_fig.png', height = 7, width = 10, units = 'in', res = 300)
+png('Figure2.png', height = 7, width = 10, units = 'in', res = 300)
 
 par(xpd = NA, mar = c(4, 6, 0, 0))
 plot(1, type = 'n', xlim = c(0, 5.5), ylim = c(0, 7.5), axes = F, xlab = NA, ylab = NA)
-segments(1:5, y0 = 0.5, y1 = 7.5)
-segments(0.5, 1:7, x1 = 5.5)
+segments(1:5, y0 = 0.5, y1 = 7.5, lty = 3)
+segments(0.5, 1:7, x1 = 5.5, lty = 3)
 symbols(x = rep(1:5, 7), y = rep(1:7, each = 5), circles = sqrt(c(t(type)))/17, inches = F, bg = rep(cols, 7), add = T)
 text(x = rep(1:5, 7), y = rep(1:7, each = 5), labels = c(t(type)))
-text(1:5, 0.25, c('1. OST initiated in\nEmergency\nDepartments\n(n=17)', '2. Addiction\nconsult teams\n(n=13)', '3. Multidisciplinary\nclinical care\n(n=3)', '4. Hospital-wide\nskills and\nprocedures\n(n=8)', 'All\ninterventions\n(n=41)'), adj = c(0.5, 1))
+text(1:5, 0.25, c('1. Pathways to initiate\nOAT in Emergency\nDepartments\n(n=17)', '2. Addiction\nConsult Teams\n(n=13)', '3. Specialist\nclinical teams\n(n=3)', '4. Hospital-wide\nskills and\nprocedures\n(n=8)', 'All\ninterventions\n(n=41)'), adj = c(0.5, 1))
 text(0.25, 1:7, element_labels, adj = 1)
 text(3, -1, 'Intervention type', font = 2)
 text(-1, 4, 'Intervention element', srt = 90, font = 2)
